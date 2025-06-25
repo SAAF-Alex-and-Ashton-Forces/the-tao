ab455310ceaea3b2d85f7ccd3ff3fc52731a3a13
alex <alexl@advanstaff.com>
2025-06-25 03:10:49 -0600

    added 1.2 & 1.3

--- src/tao.html.pm
+++ src/tao.html.pm
@@ -1,5 +1,13 @@
 #lang pollen
 
+◊; You can type the lozenge in Emacs in a few ways:
+◊;
+◊; If you have pollen-mode enabled, just type "@" and it will insert.
+◊; Type "@" twice to insert a real at-sign.
+◊;
+◊; If not, do Ctrl-\ TeX RET to enter TeX input mode, and then type \lozenge
+
+
 ◊title{The Tao of Programming}
 ◊author{Translated by Geoffrey James}
 
@@ -13,3 +21,17 @@ When you have learned to snatch the error code from the trap frame, it will be t
 
 Something mysterious is formed, born in the silent void. Waiting alone and unmoving, it is at once still and yet in constant motion. It is the source of all programs. I do not know its name, so I will
 call it the Tao of Programming.
+
+◊section{1.2}
+
+The Tao gave birth to machine language. Machine language gave birth to the assembler.
+The assembler gave birth to the compiler. Now there are ten thousand languages.
+Each language has its purpose, however humble. Each language expresses the Yin and Yang of software. Each language has its place within the Tao.
+But do not program in COBOL if you can avoid it.
+
+◊section{1.3}
+
+In the beginning was the Tao. The Tao gave birth to Space and Time.
+Therefore Space and Time are the Yin and Yang of programming.
+Programmers that do not comprehend the Tao are always running out of time and space for their programs. Programmers that comprehend the Tao always have enough time and space to accomplish their goals.
+How could it be otherwise?
