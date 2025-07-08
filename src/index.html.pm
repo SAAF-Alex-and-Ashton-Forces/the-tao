46afd5796d55fce4d9cb112a8c13abdbe0e09fcb
Ashton Wiersdorf <mail@wiersdorf.dev>
2025-07-08 16:33:59 -0600

    Add colophon

--- src/index.html.pm
+++ src/index.html.pm
@@ -15,3 +15,16 @@ A collection of hacker lore, lovingly re-typeset for the modern web.
 ◊book*{About}
 
 This is a curated collection of hacker lore scavenged from various parts of the internet. Many of these stories, sayings, etc. were written and published during the early days of the web, before the HTML 5 standard. They might look alright in a plain-text web browser, but they are uncomfortable to read on modern devices. We have carefully typeset these to look fabulous on whatever device you are reading on—be it a modern smartphone or EWW running in Emacs 24.
+
+We (the maintainers of Uncarved Bytes) do not own any of the stories; we are merely re-publishing what was already available for free online.
+
+◊saying[""]{
+…freely ye have received, freely give…
+}
+
+
+◊book*{Colophon}
+
+The body font is ◊link["https://practicaltypography.com/charter.html"]{Bitstream Charter} by Matthew Carter. The heading font is ◊link["https://www.cooperhewitt.org/open-source-at-cooper-hewitt/cooper-hewitt-the-typeface-by-chester-jenkins/"]{Cooper Hewitt} by Chester Jenkins.
+
+This site was built with ◊link["https://docs.racket-lang.org/pollen/"]{Pollen} by Matthew Butterick and other contributors. The pages and CSS were all hand-written. We used Emacs—the one true text editor—throughout the project.
