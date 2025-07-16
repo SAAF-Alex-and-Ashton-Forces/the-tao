f59fd088889474f765f4d4c55c5c8c71700ec653
Ashton Wiersdorf <mail@wiersdorf.dev>
2025-07-16 14:49:13 -0600

    Add glider icon (not yet set as favicon); placeholder for Mel

---
+++ src/mel.html.pm
@@ -0,0 +1,7 @@
+#lang pollen
+
+◊; Annotated version here:
+◊; https://users.cs.utah.edu/~elb/folklore/mel-annotated/node1.html#SECTION00010000000000000000
+◊title{The Story of Mel, A Real Programmer}
+◊author{Story by Ed Nather, Annotated by Erik Brunvand}
+
