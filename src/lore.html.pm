d42437187bb754ac9ea26739adb81a1006728ce3
Ashton Wiersdorf <mail@wiersdorf.dev>
2025-08-18 13:33:29 -0600

    Whatever

---
+++ src/lore.html.pm
@@ -0,0 +1,10 @@
+#lang pollen
+
+◊title{Hacker Lore}
+
+◊itemize-nav{
+  ◊link["/tao.html"]{◊it{The Tao of Programming}, by Geoffrey James}
+  ◊link["/perlis.html"]{◊it{Epigrams in Programming}, by Alan Perlis}
+  ◊link["/mel.html"]{◊it{The Story of Mel}, by Ed Nather}
+  ◊link["/magic.html"]{◊it{A Story About Magic}}
+}
